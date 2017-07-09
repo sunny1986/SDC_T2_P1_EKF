7f4e9ef4b5ad7cfc205fe9f98396dedac21f6f7c
sunny1986 <ps566@scarletmail.rutgers.edu>
2017-07-09 12:38:25 -0400

    Turned off sensor reading

--- src/FusionEKF.h
+++ src/FusionEKF.h
@@ -44,6 +44,12 @@ private:
   Eigen::MatrixXd R_radar_;
   Eigen::MatrixXd H_laser_;
   Eigen::MatrixXd Hj_;
+  Eigen::MatrixXd F_;
+  Eigen::MatrixXd P_;
+
+  //acceleration noise components
+  float noise_ax;
+  float noise_ay;
 };
 
 #endif /* FusionEKF_H_ */
